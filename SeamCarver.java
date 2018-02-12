5155463d653d4a225a267f3c4b041798b02a0750
Elsa Hill <ehill@newrelic.com>
2018-02-11 18:49:18 -0800

    Created digrapha and topological sort objects

--- SeamCarver.java
+++ SeamCarver.java
@@ -1,9 +1,11 @@
 import edu.princeton.cs.algs4.*;
-
 import java.awt.*;
 
 public class SeamCarver {
     private Picture pic;
+    private double[][] energyArray;
+    Digraph verticalDigraph;
+    Digraph horizontalDigraph;
 
     // mutable is a deep copy
     // imutable is a final var
@@ -11,49 +13,56 @@ public class SeamCarver {
 
     public SeamCarver(Picture picture) {                    // create a seam carver object based on the given picture
         pic = new Picture(picture);
-        double[][] energyArray = new double[pic.width()][pic.height()];
+        this.energyArray = new double[pic.width()][pic.height()];
+        this.verticalDigraph = new Digraph(height()*width());
+
 
         // Start with the lowest energy of the second... index 1 array.
-        // Square the amounts only when needed
-        //TODO: How is getRGB supposed to be used when it returns a neg int?
+        // Square the amounts only when needed?
 
-        for (int i = 0; i < pic.width(); i++) {
-            for (int j = 0; j < pic.height(); j++) {
+        createVerticalDigraph();
+        createHorizontalDigraph();
 
-                energyArray[i][j] = energy(i,j);
+        Topological topological = new Topological(verticalDigraph);
+        Iterable<Integer> topOrder = topological.order();
+        int pc = 0;
+    }
 
+    private void createHorizontalDigraph() {
+        //TODO
+    }
+
+    private void createVerticalDigraph() {
+        int vertex = 0;
+
+        for (int x = 0; x < pic.width(); x++) {
+            for (int y = 0; y < pic.height(); y++) {
+
+                energyArray[x][y] = energy(x, y);
+
+                // Adds edges on
+                if (vertex % width() == 0 && vertex + width() < width()*height()) {
+                    verticalDigraph.addEdge(vertex, vertex + width()+1);
+                    verticalDigraph.addEdge(vertex, vertex + width());
+                    vertex++;
+                }
+
+                if ((vertex + 1) % width() == 0 && vertex + width() < width()*height()) {
+                    verticalDigraph.addEdge(vertex, vertex + width()-1);
+                    verticalDigraph.addEdge(vertex, vertex+width());
+
+                    vertex++;
+                }
+
+                if ((y != 0 && y != width() - 1) && vertex + width() < width()*height()) {
+                    verticalDigraph.addEdge(vertex, vertex + width()+1);
+                    verticalDigraph.addEdge(vertex, vertex + width()-1);
+                    verticalDigraph.addEdge(vertex, vertex + width());
+                    vertex++;
+
+                }
             }
         }
-        
-
-//        Topological topologicalWidth = new Topological(digraphWidth);
-
-
-        /*---------- Memory Map ---------------
-                    Goal to accomplish next (What's this things job?):
-                          Transform/encode pixels into v with a weight directed at w
-                          Grab the energy function of each vertex and store it in an Array
-                          Traverse downward with pixel connecting to bottom three and see which is less
-
-
-                    Process - pseudo code (What we'll need):
-                          HashSet<Integer> nounIdSet = nounToSynsetIdMap.get(noun);
-                          nounIdSet.add(synsetId);
-
-                          HashSet<Integer> nounsInSynset = synsetIdToNounMap.get(noun);
-                          nounsInSynset.add(noun);
-
-                    Test Result Validation:
-                          nounToSynsetIdMap.get("appearsTwice") returns 1,3
-                          nounIdSet.get("one") returns 1,2
-*/
-
-
-        // Exercise says not to use EdgeWeightedDigraph or DirectedEdge. AcyclicSP takes an EdgeWeightedDigraph param.
-//        AcyclicSP acyclicSP = new AcyclicSP();
-//        acyclicSP.pathTo(4);
-
-
     }
 
     public Picture picture() {                              // current picture
@@ -70,44 +79,52 @@ public class SeamCarver {
         return pic.height();
     }
 
+    private int deltaHorizontal(int x, int y) {
+        Color right = pic.get(x + 1, y);
+        Color left = pic.get(x - 1, y);
+
+        int deltaRed = ((right.getRed() - left.getRed())) * ((right.getRed() - left.getRed()));
+        int deltaGreen = (right.getGreen() - left.getGreen()) * (right.getGreen() - left.getGreen());
+        int deltaBlue = (right.getBlue() - left.getBlue()) * (right.getBlue() - left.getBlue());
+
+        return deltaRed + deltaGreen + deltaBlue;
+    }
+
+    private int deltaVertical(int x, int y) {
+        Color above = pic.get(x, y - 1);
+        Color below = pic.get(x, y + 1);
+
+        int deltaRed = ((below.getRed() - above.getRed()) * (below.getRed() - above.getRed()));
+        int deltaGreen = ((below.getGreen() - above.getGreen()) * (below.getGreen() - above.getGreen()));
+        int deltaBlue = ((below.getBlue() - above.getBlue()) * (below.getBlue() - above.getBlue()));
+
+        return deltaRed + deltaGreen + deltaBlue;
+    }
+
     public double energy(int x, int y) {               // energy of pixel at column x and row y
 
         if (x == 0 || y == 0 || x == width() - 1 || y == height() - 1) {
             return 1000;
         }
 
-        //TODO: Switch to using getRGB
-        Color right = pic.get(x + 1, y);
-        Color left = pic.get(x - 1, y);
-        Color up = pic.get(x, y - 1);
-        Color below = pic.get(x, y + 1);
-
-        int[] leftRight = new int[3];
-
-        //TODO: Switch to be able to apply this to all graphs, not just 3x4 pixels.
-        leftRight[0] = (right.getRed() - left.getRed()) * (right.getRed() - left.getRed());
-        leftRight[1] = (right.getGreen() - left.getGreen()) * (right.getGreen() - left.getGreen());
-        leftRight[2] = (right.getBlue() - left.getBlue()) * (right.getBlue() - left.getBlue());
-
-        int[] upDown = new int[3];
-        upDown[0] = ((below.getRed() - up.getRed())) * ((below.getRed() - up.getRed()));
-        upDown[1] = ((below.getGreen() - up.getGreen())) * ((below.getGreen() - up.getGreen()));
-        upDown[2] = ((below.getBlue() - up.getBlue())) * ((below.getBlue() - up.getBlue()));
-
-        int sumLeftRight = 0;
-        for (int i = 0; i < leftRight.length; i++) {
-            sumLeftRight += leftRight[i];
-        }
-
-        int sumUpDown = 0;
-        for (int i = 0; i < upDown.length; i++) {
-            sumUpDown += upDown[i];
-        }
-
-        return sumLeftRight + sumUpDown;
+        return Math.sqrt(deltaHorizontal(x, y) + deltaVertical(x,y));
     }
 
+
     public int[] findVerticalSeam() {                // sequence of indices for vertical seam
+
+        // Find the lowest pixel on the second row
+        int lowestVertIndex = 0;
+        for (int i = 0; i < energyArray.length; i++) {
+            double lowerCanidate = energyArray[1][i];
+            double lowest = 0.0;
+            if (lowest > lowerCanidate ) {
+                lowestVertIndex = i;
+            }
+        }
+
+
+
 
 
         return null;
