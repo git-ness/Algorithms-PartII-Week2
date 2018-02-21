5b70283850b1ac3ded2f8943eaff7ba0730360a5
Elsa Hill <ehill@newrelic.com>
2018-02-20 21:06:48 -0800

    Code cleanup

--- SeamCarver.java
+++ SeamCarver.java
@@ -195,9 +195,7 @@ public class SeamCarver {
             double energyValue = 0;
 
             for (int i = 0; i < seam.length; i++) {
-                // 3, 0 --> location of first vertex
                 energyValue += energyArray[verticalSeamArray[i]][i];
-                // 2, 1 --> location of second vertex
 
 
             }
@@ -205,103 +203,53 @@ public class SeamCarver {
         }
     }
 
-    public int[] findHorizontalSeam() {              // sequence of indices for horizontal seam
+    private void processVerticalSeams() {
 
-
-        return null;
+        // If a seam "crosses" a different seam, this will reorder the seam to ensure they no longer "cross" over each other.
+        for (int row = 0; row < height(); row++) {
+            for (int i = 0; i < verticalSeamsToRemoveList.size(); i++) {
+                if (verticalSeamsToRemoveList.get(0)[i] > verticalSeamsToRemoveList.get(verticalSeamsToRemoveList.size() - 1 - i)[i]) {
+                    int seamValue = verticalSeamsToRemoveList.get(0)[i];
+                    verticalSeamsToRemoveList.get(0)[i] = verticalSeamsToRemoveList.get(verticalSeamsToRemoveList.size() - 1 - i)[i];
+                    verticalSeamsToRemoveList.get(verticalSeamsToRemoveList.size() - 1 - i)[i] = seamValue;
+                }
+            }
+        }
     }
+
+        public int[] findHorizontalSeam () {              // sequence of indices for horizontal seam
+
+
+            return null;
+        }
 
     public void removeHorizontalSeam(int[] seam) {  // remove horizontal seam from current picture
 
     }
 
     public void removeVerticalSeam(int[] seam) {    // remove vertical seam from current picture
-        
+        // Orders the seams so that if the seams cross each other, they are reordered from left to right in the picture
+        processVerticalSeams();
+
+        Picture newPic = new Picture(width() - verticalSeamsToRemoveList.size(), height());
+
+        for (int row = 0; row < height(); row++) {
+            for (int col = 0; col < width(); col++) {
+                for (int i = 0; i < verticalSeamsToRemoveList.size(); i++) {
+                    if (col == verticalSeamsToRemoveList.get(i)[row]) {
+                        // Do nothing
+                        col++;
+                    } else {
+                        newPic.set(col, row, pic.get(1,1));
+                        col++;
+                    }
+                }
+
+            }
+        }
+        newPic.save("newpic.jpg");
 
     }
-
-
-
-    /*---------- vertical seam removal map ---------------
-                    Goal to accomplish next (What's this things job?):
-                        To remove the seam
-                         Find the seam, add the seam to the ArrayList until removeSeam is called
-                         if (arrayList.size == 1)
-                            Create a new picture and move all pixels to the left of their current pos.
-                         else
-                            order the seams to ensure every spot is increasing.
-                            for length of arrayList
-                                Find the range of the pixels to be "saved" and moved to the left
-
-                                // Determine the last seam to the end of the picture using the below
-                                use arraylist.size() in (width() - arrayList.size())
-
-
-
-
-
-                    Process - pseudo code (What we'll need):
-                        ArrayList<int[]> verticalSeamsToRemoveList = new ArrayList<>();
-
-                          removeVerticalSeam(int[] seam) {
-                            if (verticalSeamsToRemoveList.size() == 1) {
-                                new Picture(width - 1, height())
-                                for (width - seamIndex) {
-                                    color = pic.getRGB(row+1,col)
-                                    pic.set(row,col,color)
-                                }
-
-                            } else {
-
-                                // Order the seams so they are always increasing at each column
-                                for (int i = 0; i < verticalSeamsToRemoveList.size(); i++) {
-                                        int[] newSeam0;
-                                        int[] newSeam1;
-
-                                        vertSeam1 = verticalSeamsToRemoveList.index(i);
-                                        // verticalSeamsToRemoveList.index(0);
-
-                                        vertSeam2 = verticalSeamsToRemoveList.index(i+1);
-                                        // verticalSeamsToRemoveList.index(1);
-
-                                        if (vertSeam1.index(i) > vertSeam2(i))
-                                }
-                            }
-                            pic.save();
-                          }
-
-
-
-
-                    Test Result Validation:
-
-     -------------------------------------
-
-      // Original idea
-                          Remove the seam
-                          Move RGB from the right of the seam into the space of the removed seam until the end
-                          Recalculate the energies along the seam
-                          ?When do we finish minimizing the picture via the seam? Perhaps the tests stop at a certain point.
-                          Save picture
-
-                          // Original idea
-                          for i of 0 till length take the seam and convert to picture location
-                             location on x axis = verticalSeamArray[i]
-                             colorValue = pic.getRGB(col,row)
-                             pic.set(row, col, colorValue) <-- dependent on if we can set value from getRGB here
-                               ^ Set where seem is
-                             pic.set(row, col + 1, colorValueFromRight)
-                             -- continue until end, set last pixel to null --
-                               (keep a counter to keep track of how many pixels to the right to go)
-
-                           for every element in the seamArray represented as i
-                              energyArray[verticalSeam[i]][i] = energy(verticalSeam[i],i)
-
-    */
-
-    //TODO: How do we remove the empty spaces on the right when done removing the seams?
-    //TODO: Only way I can think of is create a new array, resize it and add everything one by
-    //TODO: one but that doesn't seem right. https://tinyurl.com/seamremoval suggests it however.
 
 
     private int deltaHorizontal(int x, int y) {
